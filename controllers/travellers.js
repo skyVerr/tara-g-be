3a763e842c832d8fb420f82ba5378c1f1bde8ca5
unknown <zylegaspi@gmail.com>
2019-05-15 14:14:42 +0800

    functional challenge

--- controllers/travellers.js
+++ controllers/travellers.js
@@ -1,4 +1,8 @@
 const pool = require('../connection');
+const fetch = require('node-fetch');
+const uniqid = require('uniqid');
+const path = require('path');
+const download = require('image-downloader');
 
 class Travellers{
     static async get(req, res) {
@@ -11,6 +15,63 @@ class Travellers{
             res.json(results);
         });
     }
+
+    static async addFacebook(req, res) {
+        // console.log(req.body);
+        // let newImageName = uniqid()+'.jpg';
+        // const options = {
+        //     url: req.body.picture.data.url,
+        //     dest: './images/'+newImageName
+        // }
+           
+        // download.image(options)
+        // .then(({ filename, image }) => {
+        //     console.log('File saved to', filename)
+            let fb = {
+                id: req.body.id,
+                user_id: req.params.id,
+                name: req.body.name,
+                // picture: 'https://taragame.appspot.com/'+newImageName,
+                picture: req.body.picture.data.url
+            };
+            let sql = "INSERT INTO users_facebook SET ? ";
+            pool.query(sql, fb, (err, results) =>{
+                if(err) return res.json({message: 'existing'});
+                res.json(results);
+            });
+        // })
+        // .catch((err) => {
+        //     throw err;
+        // })
+    }
+
+    static async challengeDone(req, res){
+        let sql = "SELECT * FROM travellers_challenges WHERE user_id = ? ";
+        pool.query(sql , [req.params.id], (err, results) => {
+            if(err) throw err;
+            if(!!!results.length){
+                sql = "INSERT INTO travellers_challenges SET user_id = ? ";
+                pool.query(sql, [req.params.id], (err, results) => {
+                    if(err) throw err;
+                    res.json({message: 'joined'});
+                })
+            } else {
+                res.json({message: 'already joined'});
+            }
+        });
+    }
+
+    static async leaderboard(req, res) {
+        let sql = 
+        `SELECT users_facebook.id, users_facebook.name, users_facebook.picture 
+        FROM travellers_challenges LEFT JOIN users_facebook ON 
+        users_facebook.user_id = travellers_challenges.user_id`;
+        pool.query(sql, req.body, (err, results) =>{
+            if(err) throw err;
+            res.json(results);
+        });
+    }
+
 }
 
 module.exports = Travellers;
--- routes/travellers.js
+++ routes/travellers.js
@@ -4,5 +4,8 @@ const Auth = require('../controllers/auth.controller');
 const Travellers = require('../controllers/travellers');
 
 router.get('/', Travellers.get);
+router.post('/:id/facebook', Travellers.addFacebook);
+router.post('/:id/challenge', Travellers.challengeDone);
+router.get('/leaderboard', Travellers.leaderboard);
 
 module.exports = router;
